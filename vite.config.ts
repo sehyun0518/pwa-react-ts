c1e2607477b242324da3560ed05cd65673046048
SeHyun <rla6170@naver.com>
2025-03-21 15:10:48 +0900

    chore: path alias setting

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -45,5 +46,6 @@ export default defineConfig({
         },
       },
     }),
+    tsconfigPaths(),
   ],
 });
